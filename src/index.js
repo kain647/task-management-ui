3431468c7d9631d44d1b35898cef1ab32313c8e6
Aleksey <aleksey.amelchenko@gmail.com>
2021-03-03 13:26:20 +0300

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
 import "./index.css";
 import Task from "./task-management/index";
+
+TimeAgo.addDefaultLocale(en);
 
 ReactDOM.render(
   <React.StrictMode>
